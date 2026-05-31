e12d800d12cd14f5a7c80fbce48b2d7d9e130d7a
crazyadi29 <adichorvadi@gmail.com>
2026-05-31 14:52:25 +0530

    Update cron.py

--- cron.py
+++ cron.py
@@ -1,10 +1,8 @@
 import schedule
 import time
 import login
-
-# Every day at 8:30 AM IST = 3:00 AM UTC
-schedule.every().day.at("03:00").do(auto_token.auto_login)
-
+ 
+schedule.every().day.at("03:00").do(login.auto_login)
 print("⏰ Cron running - waiting for 8:30 AM IST daily...")
 while True:
     schedule.run_pending()
